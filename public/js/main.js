89fbc560b50f3c45f7147952a07061945514cf3c
Ian Wilson <iwilsonq@gmail.com>
2017-02-10 23:21:26 -0800

    fixed date parse issue for safari

--- public/js/main.js
+++ public/js/main.js
@@ -14,37 +14,19 @@ $(function() {
     }, 1500, 'easeInOutExpo');
     event.preventDefault();
   });
-});
 
-$(function() {
-  const deadline = '2017-3-13';
+  const deadline = '2017/3/13';
   initializeCountdown('countdown', deadline);
 
-
-  function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date());
-    let seconds = Math.floor( (t/1000) % 60 );
-    let minutes = Math.floor( (t/1000/60) % 60 );
-    let hours = Math.floor( (t/(1000*60*60)) % 24 );
-    let days = Math.floor( t/(1000*60*60*24) );
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
   function initializeCountdown(id, endtime) {
-    let countdown = document.getElementById(id);
+    var countdown = document.getElementById(id);
     var daysSpan = countdown.querySelector('.days');
     var hoursSpan = countdown.querySelector('.hours');
     var minutesSpan = countdown.querySelector('.minutes');
     var secondsSpan = countdown.querySelector('.seconds');
 
     function updateClock() {
-      let t = getTimeRemaining(endtime);
+      var t = getTimeRemaining(endtime);
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = t.hours;
       minutesSpan.innerHTML = t.minutes;
@@ -53,9 +35,22 @@ $(function() {
         clearInterval(timeinterval);
       }
     }
+    function getTimeRemaining(endtime) {
+      var t = Date.parse(endtime) - Date.parse(new Date());
+      var seconds = Math.floor( (t/1000) % 60 );
+      var minutes = Math.floor( (t/1000/60) % 60 );
+      var hours = Math.floor( (t/(1000*60*60)) % 24 );
+      var days = Math.floor( t/(1000*60*60*24) );
+
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+    }
     updateClock();
-    let timeInterval = setInterval(updateClock, 1000);
+    var timeInterval = setInterval(updateClock, 1000);
   }
-
-
 });
