f20564ed14b7b93fddc7c192fe59936cf38f9132
irulenot <anthony.i.bilic@gmail.com>
2017-07-20 16:20:00 -0700

    Hopefully this works. Replaced all files with new versions and added new pictures

--- public/js/agency.js
+++ public/js/agency.js
@@ -19,7 +19,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
@@ -29,10 +29,68 @@
             top: 100
         }
     })
-    $('#mailform .subbtn').on('click', function (event) {
+    $('#subbtn').on('click', function (event) {
         event.preventDefault();
-        $('.subbtn').fadeOut();
+        $('#subbtn').css("display", "none");
         $('#mc_embed_signup').fadeIn('slow');
     });
+
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    })
+    var $width = $(window).width();
+    function responsiveSocial(){
+      if($width < 580) {
+        $('#socialbuttons').removeClass("list-inline");
+        $("#socialbuttons").find($("a")).addClass("pull-left mobile-social")
+      }else{
+        $('#socialbuttons').addClass("list-inline")
+        $("#socialbuttons").find($("a")).removeClass("pull-left mobile-social")
+      }
+      if ($width < 777){
+        $("#products").addClass("text-center");
+        $('#products button').removeClass("pull-right");
+      }else{
+        $("#products").removeClass("text-center");
+        $('#products button').addClass("pull-right");
+      }
+
+
+      var currentItems = document.getElementById("car-inner").querySelectorAll(".item");
+      for (var i=0; i < currentItems.length; i++){
+        $(".item .col-lg-4").unwrap();
+      }
+      responsiveCarousel();
+    }
+
+
+    function responsiveCarousel(){
+      var carouselArray = $('#teamCarousel >.carousel-inner >div');
+      var count;
+      var w = window.innerWidth;
+      if(1000<= w < 1215){
+        count = 2;
+      }
+      if (w >= 1215) {
+        count = 3;
+      }
+      if (w < 1000 ){
+        count = 1;
+      }
+      for (var q = 0; q < carouselArray.length;) {
+          var ElementsToBeWrapeed = carouselArray.slice(q, q + count);
+          if (q == 0){
+            $(ElementsToBeWrapeed).wrapAll("<div class='item active' id='team-item'></div>");
+          }else{
+            $(ElementsToBeWrapeed).wrapAll("<div class='item' id='team-item'></div>");
+          }
+          q = q + count;
+      }
+    }
+    responsiveCarousel();
+    responsiveSocial();
+    $(window).resize(responsiveSocial);
+
+
 
 })(jQuery); // End of use strict
